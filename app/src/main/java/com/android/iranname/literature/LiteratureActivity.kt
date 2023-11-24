e82409ac3fbe177a87a8338790e9e9d14a4d5b48
sarvin nami <srvn0nm@gmail.com>
2023-11-24 19:12:08 +0330

    debug

--- app/src/main/java/com/android/iranname/literature/LiteratureActivity.kt
+++ app/src/main/java/com/android/iranname/literature/LiteratureActivity.kt
@@ -1,4 +1,4 @@
-package com.android.iranname.litrature
+package com.android.iranname.literature
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,9 +10,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.android.iranname.litrature.ui.theme.IranNameTheme
+import com.android.iranname.literature.ui.theme.IranNameTheme
 
-class LitratuerActivity : ComponentActivity() {
+class LiteratureActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
