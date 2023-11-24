1bd6e9da0b18f62e53c88d0d465dffb9e8543bf8
abtinz <abtinzand@gmail.com>
2023-11-24 19:02:52 +0330

    commenting!

--- app/src/main/java/com/android/iranname/landmarks/model/LandmarksDS.kt
+++ app/src/main/java/com/android/iranname/landmarks/model/LandmarksDS.kt
@@ -7,6 +7,9 @@ import com.android.iranname.commonServices.model.CommentDC
  * @param name name of landmark
  * @param description two or three lines about landmark
  * @param location location of the landmark
+ * @param foundation data of construction of this landmark
+ * @param comments list of reviews of this landmark
+ * @param relatedLandmarksDC list of similar landmarks
  */
 data class LandmarksDC(
     val image:String,
