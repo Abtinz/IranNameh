3a50e27a6196cde0d275759b5e10ee9d9423f727
abtinz <abtinzand@gmail.com>
2023-11-24 22:13:37 +0330

    commenting!

---
+++ app/src/main/java/com/android/iranname/landmarks/ui/screen/LandmarksList.kt
@@ -0,0 +1,9 @@
+package com.android.iranname.landmarks.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun LandmarksListScreens(navController: NavController) {
+
+}
---
+++ app/src/main/java/com/android/iranname/landmarks/ui/screen/landmarks/LandmarkScreen.kt
@@ -0,0 +1,10 @@
+package com.android.iranname.landmarks.ui.screen.landmarks
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.android.iranname.landmarks.model.LandmarksDC
+
+@Composable
+fun LandmarkScreens(navController: NavController, landmarksDC: LandmarksDC) {
+
+}
