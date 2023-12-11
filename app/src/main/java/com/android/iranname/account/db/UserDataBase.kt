5acc2c654e1f60970fe3ea1e0828efdcaa388299
sarvin nami <srvn0nm@gmail.com>
2023-12-11 09:04:56 +0330

    edit room data base

--- app/src/main/java/com/android/iranname/account/db/UserDataBase.kt
+++ app/src/main/java/com/android/iranname/account/db/UserDataBase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities = [User::class],
-    version = 4,
+    version = 5,
     exportSchema = false
 )
 abstract class UserDataBase:RoomDatabase() {
