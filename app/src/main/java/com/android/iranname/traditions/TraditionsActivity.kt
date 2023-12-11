c6482a592b08e0ebe40eb74a73e902fb0e0bc1c5
sarvin nami <srvn0nm@gmail.com>
2023-12-11 04:18:01 +0330

    TraditionActivity

--- app/src/main/java/com/android/iranname/traditions/TraditionsActivity.kt
+++ app/src/main/java/com/android/iranname/traditions/TraditionsActivity.kt
@@ -3,13 +3,26 @@ package com.android.iranname.traditions
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.DrawerValue
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberDrawerState
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
+import com.android.iranname.commonServices.ui.compose.actionbar.AppActionBar
+import com.android.iranname.traditions.model.navigation.TraditionNavGraph
 import com.android.iranname.traditions.ui.theme.IranNameTheme
 
 class TraditionsActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -19,7 +32,21 @@ class TraditionsActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    val scaffoldState =
+                        rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
+                    rememberCoroutineScope()
+                    val navController = rememberNavController()
+                    Scaffold(
+                        scaffoldState = scaffoldState,
+                        topBar = {
+                            AppActionBar("آداب و رسوم")
+                        },
+                        drawerElevation = 30.dp,
+                    ) { padding ->
+                        Box(modifier = Modifier.padding(padding)) {
+                            TraditionNavGraph(navHostController = navController)
+                        }
+                    }
                 }
             }
         }
