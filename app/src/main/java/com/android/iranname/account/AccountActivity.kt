6e7c8f08586831302cc232d4661e447d90402216
sarvin nami <srvn0nm@gmail.com>
2023-12-11 04:08:53 +0330

    AccountActivity

--- app/src/main/java/com/android/iranname/account/AccountActivity.kt
+++ app/src/main/java/com/android/iranname/account/AccountActivity.kt
@@ -1,46 +1,57 @@
 package com.android.iranname.account
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.DrawerValue
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberDrawerState
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.android.iranname.account.ui.theme.IranNameTheme
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
+import com.android.iranname.account.ui.theme.AccountTheme
+import com.android.iranname.commonServices.navigation.AccountNavGraph
+import com.android.iranname.commonServices.ui.compose.actionbar.AppActionBar
 
 class AccountActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            IranNameTheme {
+            AccountTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting2("Android")
+                    val scaffoldState =
+                        rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
+                    rememberCoroutineScope()
+                    val navController = rememberNavController()
+                    Scaffold(
+                        scaffoldState = scaffoldState,
+                        topBar = {
+                            AppActionBar("احراز هویت")
+                        },
+                        drawerElevation = 30.dp,
+                    ) { padding ->
+                        Box(modifier = Modifier.padding(padding)) {
+                            AccountNavGraph(navController)
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    IranNameTheme {
-        Greeting2("Android")
     }
 }
