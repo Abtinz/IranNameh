6426d7bda1df650e73c80f0ecb8e749b0bb24c0e
sarvin nami <srvn0nm@gmail.com>
2023-12-11 09:01:43 +0330

    edit response

--- app/src/main/java/com/android/iranname/commonServices/network/response/AuthenticationResponse.kt
+++ app/src/main/java/com/android/iranname/commonServices/network/response/AuthenticationResponse.kt
@@ -2,6 +2,6 @@ package com.android.iranname.commonServices.network.response
 
 data class AuthenticationResponse(
     val message : String?,
-    val user_id : String ,
+    val user_id : Int ,
     val success:Boolean
 )
