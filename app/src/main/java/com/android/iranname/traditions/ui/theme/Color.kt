3379e27788c2bee0fb0e5dbf212ab320228c15ab
Abtinz <abtinzandi@gmail.com>
2024-01-03 23:35:05 +0330

    new colors are added

--- app/src/main/java/com/android/iranname/traditions/ui/theme/Color.kt
+++ app/src/main/java/com/android/iranname/traditions/ui/theme/Color.kt
@@ -2,15 +2,8 @@ package com.android.iranname.traditions.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
-val Cream = Color(0xFFF6F4EB)
-val LightBlue = Color(0xFF91C8E4)
-val SemiLightBlue = Color(0xFF749BC2)
-val DarkBlur = Color(0xFF4682A9)
+val primary = Color(0xFFDF2E38)
+val secondary = Color(0xFF5D9C59)
+val informationText = Color(0xFF36683A)
+val tertiary = Color(0xFFDDF7E3)
+val mainBackGround = Color(0xFFDDF7E3)
