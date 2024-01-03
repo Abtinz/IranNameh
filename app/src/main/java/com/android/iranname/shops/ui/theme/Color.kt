dadd8591df8443adca7cdec1741cd2195a842a9c
Abtinz <abtinzandi@gmail.com>
2024-01-03 23:07:34 +0330

    last color changes

--- app/src/main/java/com/android/iranname/shops/ui/theme/Color.kt
+++ app/src/main/java/com/android/iranname/shops/ui/theme/Color.kt
@@ -5,5 +5,5 @@ import androidx.compose.ui.graphics.Color
 val primary = Color(0xFF7D7463)
 val secondary = Color(0xFFFE0000)
 val informationText = Color(0xFF2E3840)
-val tertiary = Color(0xFFA8A196)
-val mainBackGround = Color(0xFFAFAFAE)
+val tertiary = Color(0xFFB6B6B6)
+val mainBackGround = Color(0xFFE0E0E0)
