597b7472278b9a00f5118360b1f3996694cfed34
Abtinz <abtinzandi@gmail.com>
2024-01-04 17:22:09 +0330

    faqs entity class is implemented

---
+++ app/src/main/java/com/android/iranname/shops/db/faqs/ProductFAQsDC.kt
@@ -0,0 +1,15 @@
+package com.android.iranname.shops.db.faqs
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity
+data class ProductFAQsDC(
+    val question : String,
+    val  answer :String,
+    val productId :String
+):Serializable{
+    @PrimaryKey(autoGenerate = true)
+    var id :Int = 0
+}
