4e1158252971e513c1fbd1dc6e7152c7693e2bf2
Abtinz <abtinzandi@gmail.com>
2024-01-05 00:20:59 +0330

    navigation mode for Tradition Screens

--- app/src/main/java/com/android/iranname/traditions/ui/model/navigation/TraditionNavGraph.kt
+++ app/src/main/java/com/android/iranname/traditions/ui/model/navigation/TraditionNavGraph.kt
@@ -3,11 +3,17 @@ package com.android.iranname.traditions.ui.model.navigation
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.android.iranname.traditions.db.CityTraditionsDC
 import com.android.iranname.traditions.ui.model.screen.TraditionScreen
 import com.android.iranname.traditions.ui.screens.CityScreen
-import com.android.iranname.traditions.ui.screens.TraditionScreen
+import com.android.iranname.traditions.ui.screens.CityTraditionsScreen
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
+import java.net.URLDecoder
 
 @ExperimentalMaterialApi
 @Composable
@@ -21,12 +27,25 @@ fun TraditionNavGraph(navHostController: NavHostController) {
             CityScreen(navHostController)
         }
 
-        composable(route = TraditionScreen.Tradition.route) { backStackEntry ->
-            val cityName = backStackEntry.arguments?.getString("cityName")
+        composable(
+            route = TraditionScreen.Tradition.route,
+            arguments = listOf(
+                navArgument("city") {
+                    type = NavType.StringType
+                }
+            )
+        ){ backStackEntry ->
+            val cityJson = backStackEntry.arguments?.getString("city")
 
-            if (cityName != null) {
-                TraditionScreen(cityName = cityName)
-            }
+            val moshi = Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build()
+            val decodedInfo = URLDecoder.decode(cityJson, "UTF-8")
+            val jsonAdapter = moshi.adapter(CityTraditionsDC::class.java).lenient()
+            val city = jsonAdapter.fromJson(decodedInfo!!)!!
+            CityTraditionsScreen(
+                city = city
+            )
         }
     }
 }
--- app/src/main/java/com/android/iranname/traditions/ui/model/screen/screen.kt
+++ app/src/main/java/com/android/iranname/traditions/ui/model/screen/screen.kt
@@ -1,5 +1,10 @@
 package com.android.iranname.traditions.ui.model.screen
 
+import com.android.iranname.traditions.db.CityTraditionsDC
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import java.net.URLEncoder
+
 const val cityScreenRoute = "CITY"
 const val traditionScreenRoute = "TRADITION"
 
@@ -7,9 +12,14 @@ const val traditionScreenRoute = "TRADITION"
 sealed class TraditionScreen(val route: String) {
     object City : TraditionScreen(route = cityScreenRoute)
 
-    object Tradition : TraditionScreen(route = "$traditionScreenRoute/{cityName}") {
-        fun passInfo(cityName: String): String {
-            return "$traditionScreenRoute/${cityName}"
+    object Tradition : TraditionScreen(route = "$traditionScreenRoute/{city}") {
+        fun passInfo(city: CityTraditionsDC): String {
+            val moshi = Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build()
+            val jsonAdapter = moshi.adapter(CityTraditionsDC::class.java).lenient()
+            val cityJson = URLEncoder.encode(jsonAdapter.toJson(city), "UTF-8")
+            return "$traditionScreenRoute/${cityJson}"
         }
     }
 }
