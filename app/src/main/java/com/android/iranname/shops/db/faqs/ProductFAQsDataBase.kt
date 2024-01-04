151989d9fccd2f172918ae76cec50c353124482e
Abtinz <abtinzandi@gmail.com>
2024-01-04 17:21:50 +0330

    faqs database manager is implemented

---
+++ app/src/main/java/com/android/iranname/shops/db/faqs/ProductFAQsDataBase.kt
@@ -0,0 +1,37 @@
+package com.android.iranname.shops.db.faqs
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [ProductFAQsDC::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class ProductFAQsDataBase:RoomDatabase() {
+    abstract fun getProductFAQsDao(): ProductFAQsDao
+
+    companion object{
+
+        @Volatile private var instance : ProductFAQsDataBase? = null
+
+        private val LOCK = Any()
+
+        private fun dbBuilder(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            ProductFAQsDataBase::class.java,
+            "productFAQsDB"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK){
+            instance ?: dbBuilder(context).also {
+                instance = it
+            }
+        }
+    }
+
+}
