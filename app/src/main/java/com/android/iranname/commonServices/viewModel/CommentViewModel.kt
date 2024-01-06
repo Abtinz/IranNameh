0013fc89d08fee5958b30885f4e9a813d9ee2798
sarvin nami <srvn0nm@gmail.com>
2024-01-06 04:39:55 +0330

    landmark comment view model

--- app/src/main/java/com/android/iranname/commonServices/viewModel/CommentViewModel.kt
+++ app/src/main/java/com/android/iranname/commonServices/viewModel/CommentViewModel.kt
@@ -1,48 +1,81 @@
 package com.android.iranname.commonServices.viewModel
 
-import androidx.compose.runtime.mutableStateOf
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.android.iranname.account.db.User
+import com.android.iranname.account.db.UserDataBase
 import com.android.iranname.commonServices.model.CommentDC
 import com.android.iranname.commonServices.network.RetrofitClient.retrofit
 import com.android.iranname.commonServices.network.url.CommentService
+import com.android.iranname.commonServices.ui.compose.comments.CommentDataBase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class CommentViewModel : ViewModel() {
 
-    val commentsState = mutableStateOf<List<CommentDC>>(emptyList())
-    private val errorState = mutableStateOf<String?>(null)
-    private val addCommentState = mutableStateOf<String?>(null)
+    private val _commentsState  = MutableStateFlow<List<CommentDC>>(emptyList())
+    val commentsState: StateFlow<List<CommentDC>> get() = _commentsState
 
-    private suspend fun getComments(landmark_id: Int) {
-        try{
-            //building api service with its headers and basic url and instances
-            val retrofitService = retrofit.create(CommentService::class.java)
+    private val _commentsState2  = MutableStateFlow<List<CommentDC>>(emptyList())
+    val commentsState2: StateFlow<List<CommentDC>> get() = _commentsState2
 
-            commentsState.value = retrofitService.getComments(landmark_id).comments
-        } catch (e: Exception) {
-            errorState.value = e.message
-        }
-    }
+    private val _addCommentState = MutableStateFlow("")
+    val addCommentState: StateFlow<String> get() = _addCommentState
+
+
 
     // Launch the coroutine when needed
-    fun fetchComments(landmark_id: Int) {
+    fun fetchComments(landmark_id: Int, context: Context) {
         viewModelScope.launch {
-            getComments(landmark_id)
+            try {
+                //building api service with its headers and basic url and instances
+                _commentsState2.value = CommentDataBase(context).getCommentDao().getComments(landmark_id)
+                val retrofitService = retrofit.create(CommentService::class.java)
+
+                _commentsState.value = retrofitService.getComments(landmark_id).comments
+            } catch (e: Exception) {
+                _addCommentState.value = e.message.toString()
+            }
         }
     }
 
-    fun addComment(comment: CommentDC) {
+    fun addComment(landmark_id: Int, text: String, context: Context) {
+        var user: User?
+        var comment: CommentDC? = null
         viewModelScope.launch {
+            try {
+                user = UserDataBase(context).getUserDao().getFirstUser()
+
+                comment = user?.let {
+                    CommentDC(
+                        user_id = it.uuid,
+                        landmark_id = landmark_id,
+                        text = text,
+                        created_at = LocalDateTime.now()
+                            .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))
+                    )
+                }
+                comment?.let { CommentDataBase(context).getCommentDao().newComment(it) }
+                comment?.let { fetchComments(it.landmark_id, context) }
+
+            } catch (e: Exception) {
+                _addCommentState.value = e.message.toString()
+                e.printStackTrace()
+            }
             try {
                 val retrofitService = retrofit.create(CommentService::class.java)
 
-                addCommentState.value = retrofitService.postComment(comment).message
+                _addCommentState.value = comment?.let { retrofitService.postComment(it).message }.toString()
                 // Optionally, you can reload the comments after adding a new one
-                getComments(comment.landmark_id)
+                comment?.let { fetchComments(it.landmark_id, context) }
             } catch (e: Exception) {
                 // Handle the error
-                errorState.value = e.message
+                _addCommentState.value = e.message.toString()
+                e.printStackTrace()
             }
         }
     }
