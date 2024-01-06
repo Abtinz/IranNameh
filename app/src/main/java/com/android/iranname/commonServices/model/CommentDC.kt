31052a12c9c7fc8d1c71c34894c7d51972b1bfeb
sarvin nami <srvn0nm@gmail.com>
2024-01-06 04:39:40 +0330

    landmark comment data class

--- app/src/main/java/com/android/iranname/commonServices/model/CommentDC.kt
+++ app/src/main/java/com/android/iranname/commonServices/model/CommentDC.kt
@@ -1,8 +1,16 @@
 package com.android.iranname.commonServices.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity
 data class CommentDC(
     val user_id: Int,
     val landmark_id: Int,
     val text:String,
     val created_at: String
-)
+): Serializable {
+    @PrimaryKey(autoGenerate = true)
+    var id :Int = 0
+}
